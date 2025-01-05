fc45b2136964d7522d06f046c4a8ca2a0129cfc7
Jimmy Liu <jimmyliu20040@gmail.com>
2025-01-05 15:35:19 -0800

    Info button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 import SideBar from "./components/SideBar"
 
 function App() {
-  return (
-    <>
-      <SideBar></SideBar>
-      <Main />
-      <Footer />
-    </>
-  )
+	const [showModal, setShowModal] = useState(false)
+
+	function handleShowModal() {
+		setShowModal(!showModal)
+	}
+
+	return (
+		<>
+			<Main />
+			{showModal && (
+			 	<SideBar />
+			)}
+			<Footer handleShowModal={handleShowModal} />
+		</>
+	)
 }
 
 export default App
